bc2ef2245fe207903681f62c88f0db8fc474e7ea
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-28 17:22:19 -0600

    tryinf to learn hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react';
 
+function Todo({ todo, index}) {
+  return <div className="todo">{todo.text}</div>;
+}
+
 function App() {
-    const [todos, setTodos] = useStae([
+
+    const [todos, setTodos] = useState([
       {
         text: 'Learn about react',
         isCompleted: false
       },
+      {
+        text: 'Meet friend for lunch',
+        isCompleted: false
+      },
+      {
+        text: 'Build really cool todo app',
+        isCompleted: false
+      },
     ])
+
+    return(
+      <div className="app">
+        <div classname="todo-list">
+          {todos.map((todo, index) => (
+            <Todo key={index} index={index} todo={todo}/>
+          ))}
+        </div>
+      </div>
+    )
   
 }
 
