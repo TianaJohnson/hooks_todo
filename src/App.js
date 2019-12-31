096151876a627efe03f18bc04360c7f3c42b3b93
Tiana Johnson <tiana.marie013@gmail.com>
2019-12-31 15:47:12 -0600

    function build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,23 @@ function Todo({ todo, index}) {
   return <div className="todo">{todo.text}</div>;
 }
 function TodoForm({addTodo}){
-  const [value, setValue] = userState('';)
+  const [value, setValue] = userState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+             className="input"
+             value={value} 
+             onChange={e => setValue(e.target.value)}/>
+    </form>
+  )
 }
 
 function App() {
