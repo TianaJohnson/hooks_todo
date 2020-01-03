548e7129ae5469e8fe6a034c87504d1e1d6a5e91
Tiana Johnson <tiana.marie013@gmail.com>
2020-01-02 21:24:02 -0600

    missed a cample case in className, rookie

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function Todo({ todo, index, completeTodo, removeTodo }) {
   // this is what creates the line through the word. is that a js method or hook?
     return (  
-              <div  className="todo" style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
-
+              <div  
+              className="todo" 
+              style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}
+            >
               {todo.text}
 
               <div>
@@ -16,13 +18,13 @@ function Todo({ todo, index, completeTodo, removeTodo }) {
     );
 } 
 function TodoForm({addTodo}){
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
-    if(!value) return;
+    if (!value) return;
     addTodo(value);
-    setValue('');
+    setValue("");
   }
 
   return (
@@ -68,115 +70,28 @@ function App() {
       const newTodos = [...todos];
       newTodos.splice(index, 1);
       setTodos(newTodos);
-    }
+    };
 
 
-    return(
+    return (
       <div className="app">
-        <div classname="todo-list">
+        <div className="todo-list">
           {todos.map((todo, index) => (
-            <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} removeTodo={removeTodo}/>
+            <Todo 
+                  key={index}
+                  index={index} 
+                  todo={todo} 
+                  completeTodo={completeTodo} 
+                  removeTodo={removeTodo}
+                  />
           ))}
-          <TodoForm addTodo ={addTodo}/>
+          <TodoForm addTodo ={addTodo} />
         </div>
       </div>
-    )
+    );
   
 }
 
 export default App;
 
 
-// import React, { useState } from "react";
-// import "./App.css";
-
-// function Todo({ todo, index, completeTodo, removeTodo }) {
-//   return (
-//     <div
-//       className="todo"
-//       style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-//     >
-//       {todo.text}
-
-//       <div>
-//         <button onClick={() => completeTodo(index)}>Complete</button>
-//         <button onClick={() => removeTodo(index)}>x</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// function TodoForm({ addTodo }) {
-//   const [value, setValue] = useState("");
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     if (!value) return;
-//     addTodo(value);
-//     setValue("");
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         type="text"
-//         className="input"
-//         value={value}
-//         onChange={e => setValue(e.target.value)}
-//       />
-//     </form>
-//   );
-// }
-
-// function App() {
-//   const [todos, setTodos] = useState([
-//     {
-//       text: "Learn about React",
-//       isCompleted: false
-//     },
-//     {
-//       text: "Meet friend for lunch",
-//       isCompleted: false
-//     },
-//     {
-//       text: "Build really cool todo app",
-//       isCompleted: false
-//     }
-//   ]);
-
-//   const addTodo = text => {
-//     const newTodos = [...todos, { text }];
-//     setTodos(newTodos);
-//   };
-
-//   const completeTodo = index => {
-//     const newTodos = [...todos];
-//     newTodos[index].isCompleted = true;
-//     setTodos(newTodos);
-//   };
-
-//   const removeTodo = index => {
-//     const newTodos = [...todos];
-//     newTodos.splice(index, 1);
-//     setTodos(newTodos);
-//   };
-
-//   return (
-//     <div className="app">
-//       <div className="todo-list">
-//         {todos.map((todo, index) => (
-//           <Todo
-//             key={index}
-//             index={index}
-//             todo={todo}
-//             completeTodo={completeTodo}
-//             removeTodo={removeTodo}
-//           />
-//         ))}
-//         <TodoForm addTodo={addTodo} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
